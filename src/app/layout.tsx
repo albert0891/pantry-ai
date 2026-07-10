67cc95bda3e46a77c94a83ddd3716c6a5190dc23
Albert <albert0891@gmail.com>
2026-07-10 19:16:42 -0400

    chore: Remove local impeccable live script from layout to fix production console error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,6 @@ export default function RootLayout({
         <AuthProvider>
           <ApolloProvider>{children}</ApolloProvider>
         </AuthProvider>
-        {/* impeccable-live-start */}
-        <script src="http://localhost:8400/live.js"></script>
-        {/* impeccable-live-end */}
       </body>
     </html>
   );
